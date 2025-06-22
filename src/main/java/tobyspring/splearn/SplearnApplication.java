404df3acc378ebc01c658a7dc28ed6f37ef60d67
ykmxxi <ykmxxi97@gmail.com>
2025-06-22 18:34:08 +0900

    refactor: 애플리케이션 네이밍 변경

--- src/main/java/tobyspring/splearn/SplearnApplication.java
+++ src/main/java/tobyspring/splearn/SplearnApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TobyCleanSpringSplearnApplication {
+public class SplearnApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(TobyCleanSpringSplearnApplication.class, args);
+        SpringApplication.run(SplearnApplication.class, args);
     }
 
 }
