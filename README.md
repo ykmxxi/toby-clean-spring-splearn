2e6e7d4681ab214a794f3ed1c195551a4c20d218
ykmxxi <ykmxxi97@gmail.com>
2025-06-23 21:56:56 +0900

    build: docker mysql 설정 변경

---
+++ README.md
@@ -0,0 +1,14 @@
+# 🚀 SDKMAN JDK Version
+
+- SDKMAN으로 지정된 JDK Version 설치
+
+```shell
+sdk env install
+```
+
+- 프로젝트 폴더에 들어갈 때 자동으로 저장된 JDK Version 활성화
+
+```shell
+~/.sdkman/etc/config  # 의 다음 줄을 수정
+sdkman_auto_env=true 
+```
--- build.gradle.kts
+++ build.gradle.kts
@@ -26,11 +26,15 @@ repositories {
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-web")
+
     compileOnly("org.projectlombok:lombok")
     developmentOnly("org.springframework.boot:spring-boot-docker-compose")
     runtimeOnly("com.h2database:h2")
     runtimeOnly("com.mysql:mysql-connector-j")
+
     annotationProcessor("org.projectlombok:lombok")
+    testAnnotationProcessor("org.projectlombok:lombok")
+
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
